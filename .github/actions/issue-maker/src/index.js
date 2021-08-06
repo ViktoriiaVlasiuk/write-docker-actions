d0d5cf26e8b04744a042209d61668bdceeb3531e
ViktoriiaVlasiuk <59017270+ViktoriiaVlasiuk@users.noreply.github.com>
2021-08-06 17:25:43 +0300

    Update index.js

--- .github/actions/issue-maker/src/index.js
+++ .github/actions/issue-maker/src/index.js
@@ -7,8 +7,8 @@ async function run() {
 
   const token = core.getInput("repoToken");
   try {
-    const octokit = github.GitHub(token);
-
+    const octokit = github.getOctokit(token);
+    
     const newIssue = await octokit.rest.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
